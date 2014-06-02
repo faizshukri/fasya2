1f4a84e1130de1ad2e6bb49a76fc45ab1eb6369b
Faiz Shukri <faizshukri90@gmail.com>
2014-06-02 14:20:40 +0800

    Add container class only if not in home

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,14 @@
 'use strict';
 
 angular.module('fasyaApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $location) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.isHome = function(){
+        return $location.path() == '/';
+    }
   });
