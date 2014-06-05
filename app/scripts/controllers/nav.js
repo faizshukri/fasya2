7eb37e2eab8089e03e46295471605f07025ce996
Faiz Shukri <faizshukri90@gmail.com>
2014-06-06 01:21:00 +0800

    Fix close nav on brand click

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -8,8 +8,13 @@ angular.module('fasyaApp')
         };
 
         $scope.init = function(){
-            var collapse = angular.element('.navbar-collapse');
+            var collapse = angular.element('.navbar-collapse'),
+                header = angular.element('.navbar-header');
             collapse.delegate('li a', 'click', function(e){
+                if(collapse.hasClass('in')) collapse.collapse('toggle');
+            });
+
+            header.delegate('.navbar-brand','click', function(e){
                 if(collapse.hasClass('in')) collapse.collapse('toggle');
             });
         }
