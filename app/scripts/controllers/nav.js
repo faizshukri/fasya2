158f5bffa85b16dd9cb9617fa87634d2785b4ff0
Faiz Shukri <faizshukri90@gmail.com>
2014-06-06 01:08:50 +0800

    Fix close navigation in collapsed mode

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -7,4 +7,13 @@ angular.module('fasyaApp')
             return viewLocation === $location.path();
         };
 
+        $scope.init = function(){
+            var collapse = angular.element('.navbar-collapse');
+            collapse.delegate('li a', 'click', function(e){
+                if(collapse.hasClass('in')) collapse.collapse('toggle');
+            });
+        }
+
+        $scope.init();
+
     }]);
